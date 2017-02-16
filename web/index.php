7da711c6f06825be37c21849e06ba5ec56092911
fhuszti <f.huszti@gmail.com>
2017-02-16 09:48:26 +0100

    Third iteration - Installing Silex

--- web/index.php
+++ web/index.php
@@ -1,14 +1,22 @@
 <?php
-//Data access
-require 'model.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
-//fetching the articles
-$articles = getArticles();
+$app = new Silex\Application();
+
+// enable the debug mode
+$app['debug'] = true;
+
+require __DIR__.'/../app/routes.php';
+
+$app->run();
+
+
+
+
+
+
 
 //converting dates to european format
 //not doing it in the SQL request so we can display machine readable date in <time> tag
 $datePosted = (new DateTime($article['art_date']))->format('d/m/Y');
 $dateLastModif = (new DateTime($article['art_last_modif']))->format('d/m/Y');
-
-//Data display
-require 'view.php';
