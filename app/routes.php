ccd872e63ed84b6fe46bad8110536a6807c616dc
fhuszti <f.huszti@gmail.com>
2017-02-19 16:20:59 +0100

    Reorganized controllers

--- app/routes.php
+++ app/routes.php
@@ -1,218 +1,40 @@
 <?php
-use Symfony\Component\HttpFoundation\Request;
-use MicroCMS\Domain\Article;
-use MicroCMS\Domain\Comment;
-use MicroCMS\Domain\User;
-use MicroCMS\Form\Type\ArticleType;
-use MicroCMS\Form\Type\CommentType;
-use MicroCMS\Form\Type\UserType;
-
-
-
-
 // Home page
-$app->get('/', function () use ($app) {
-    $articles = $app['dao.article']->findAll();
-
-    return $app['twig']->render('index.html.twig', array('articles' => $articles));
-})->bind('home');
-
-
-
+$app->get('/', "MicroCMS\Controller\HomeController::indexAction")->bind('home');
 
 //Individual article pages with comments
 //match() for POST+GET
-$app->match('/article/{id}', function($id, Request $request) use ($app) {
-    $article = $app['dao.article']->find($id);
-
-    $commentFormView = null;
-    // A user is fully authenticated : he can add comments
-    if ($app['security.authorization_checker']->isGranted('IS_AUTHENTICATED_FULLY')) {
-        $user = $app['user'];
-
-        //Generate the comment
-        $comment = new Comment();
-        $comment->setArticle($article);
-        $comment->setAuthor($user);
-
-        //Generate the form going with the comment
-        $commentForm = $app['form.factory']->create(CommentType::class, $comment);
-        //Manage form submission
-        $commentForm->handleRequest($request);
-        if($commentForm->isSubmitted() && $commentForm->isValid()) {
-            $app['dao.comment']->save($comment);
-            $app['session']->getFlashBag()->add('success', 'Votre commentaire a été ajouté avec succès.');
-        }
-
-        //Addind form view to the page
-        //variable == null is user not logged in
-        $commentFormView = $commentForm->createView();
-    }
-
-    $comments = $app['dao.comment']->findAllByArticle($id);
-
-    return $app['twig']->render('article.html.twig', array(
-        'article' => $article,
-        'comments' => $comments,
-        'commentForm' => $commentFormView
-    ));
-})->bind('article');
-
-
-
+$app->match('/article/{id}', "MicroCMS\Controller\HomeController::articleAction")->bind('article');
 
 //Login page
-$app->get('/login', function(Request $request) use ($app) {
-    return $app['twig']->render('login.html.twig', array(
-        'error' => $app['security.last_error']($request),
-        'last_username' => $app['session']->get('_security.last_username')
-    ));
-})->bind('login');
+$app->get('/login', "MicroCMS\Controller\HomeController::loginAction")->bind('login');
 
 
 
 
 //Administration page
-$app->get('/admin', function() use ($app) {
-    $articles = $app['dao.article']->findAll();
-    $comments = $app['dao.comment']->findAll();
-    $users = $app['dao.user']->findAll();
-
-    return $app['twig']->render('admin.html.twig', array(
-        'articles' => $articles,
-        'comments' => $comments,
-        'users' => $users
-    ));
-})->bind('admin');
+$app->get('/admin', "MicroCMS\Controller\AdminController::indexAction")->bind('admin');
 
 // Add a new article
-$app->match('/admin/article/add', function(Request $request) use ($app) {
-    $article = new Article();
-
-    $articleForm = $app['form.factory']->create(ArticleType::class, $article);
-    $articleForm->handleRequest($request);
-    if ($articleForm->isSubmitted() && $articleForm->isValid()) {
-        $app['dao.article']->save($article);
-        $app['session']->getFlashBag()->add('success', 'L\'article a été créé avec succès.');
-    }
-
-    return $app['twig']->render('article_form.html.twig', array(
-        'title' => 'Créer un article',
-        'articleForm' => $articleForm->createView()));
-})->bind('admin_article_add');
+$app->match('/admin/article/add', "MicroCMS\Controller\AdminController::addArticleAction")->bind('admin_article_add');
 
 // Edit an existing article
-$app->match('/admin/article/{id}/edit', function($id, Request $request) use ($app) {
-    $article = $app['dao.article']->find($id);
-
-    $articleForm = $app['form.factory']->create(ArticleType::class, $article);
-    $articleForm->handleRequest($request);
-    if ($articleForm->isSubmitted() && $articleForm->isValid()) {
-        $app['dao.article']->save($article);
-        $app['session']->getFlashBag()->add('success', 'L\'article a été modifié avec succès.');
-    }
-
-    return $app['twig']->render('article_form.html.twig', array(
-        'title' => 'Modifier l\'article',
-        'articleForm' => $articleForm->createView()));
-})->bind('admin_article_edit');
+$app->match('/admin/article/{id}/edit', "MicroCMS\Controller\AdminController::editArticleAction")->bind('admin_article_edit');
 
 // Remove an article
-$app->get('/admin/article/{id}/delete', function($id, Request $request) use ($app) {
-    // Delete the article
-    $app['dao.article']->delete($id);
-    $app['session']->getFlashBag()->add('success', 'L\'article a été supprimé avec succès.');
-
-    // Redirect to admin home page
-    return $app->redirect($app['url_generator']->generate('admin'));
-})->bind('admin_article_delete');
+$app->get('/admin/article/{id}/delete', "MicroCMS\Controller\AdminController::deleteArticleAction")->bind('admin_article_delete');
 
 // Edit an existing comment
-$app->match('/admin/comment/{id}/edit', function($id, Request $request) use ($app) {
-    $comment = $app['dao.comment']->find($id);
-
-    $commentForm = $app['form.factory']->create(CommentType::class, $comment);
-    $commentForm->handleRequest($request);
-    if ($commentForm->isSubmitted() && $commentForm->isValid()) {
-        $app['dao.comment']->save($comment);
-        $app['session']->getFlashBag()->add('success', 'Le commentaire a été modifié avec succès.');
-    }
-
-    return $app['twig']->render('comment_form.html.twig', array(
-        'title' => 'Modifier le commentaire',
-        'commentForm' => $commentForm->createView()));
-})->bind('admin_comment_edit');
+$app->match('/admin/comment/{id}/edit', "MicroCMS\Controller\AdminController::editCommentAction")->bind('admin_comment_edit');
 
 // Remove a comment
-$app->get('/admin/comment/{id}/delete', function($id, Request $request) use ($app) {
-    $app['dao.comment']->delete($id);
-    $app['session']->getFlashBag()->add('success', 'Le commentaire a été supprimé avec succès.');
-
-    // Redirect to admin home page
-    return $app->redirect($app['url_generator']->generate('admin'));
-})->bind('admin_comment_delete');
+$app->get('/admin/comment/{id}/delete', "MicroCMS\Controller\AdminController::deleteCommentAction")->bind('admin_comment_delete');
 
 // Add a user
-$app->match('/admin/user/add', function(Request $request) use ($app) {
-    $user = new User();
-
-    $userForm = $app['form.factory']->create(UserType::class, $user);
-    $userForm->handleRequest($request);
-    if ($userForm->isSubmitted() && $userForm->isValid()) {
-        // generate a random salt value
-        $salt = substr(md5(time()), 0, 23);
-
-        $user->setSalt($salt);
-
-        $plainPassword = $user->getPassword();
-
-        // find the default encoder
-        $encoder = $app['security.encoder.bcrypt'];
-
-        // compute the encoded password
-        $password = $encoder->encodePassword($plainPassword, $user->getSalt());
-        $user->setPassword($password);
-
-        $app['dao.user']->save($user);
-        $app['session']->getFlashBag()->add('success', 'Le membre a été créé avec succès.');
-    }
-
-    return $app['twig']->render('user_form.html.twig', array(
-        'title' => 'Nouveau membre',
-        'userForm' => $userForm->createView()));
-})->bind('admin_user_add');
+$app->match('/admin/user/add', "MicroCMS\Controller\AdminController::addUserAction")->bind('admin_user_add');
 
 // Edit an existing user
-$app->match('/admin/user/{id}/edit', function($id, Request $request) use ($app) {
-    $user = $app['dao.user']->find($id);
-
-    $userForm = $app['form.factory']->create(UserType::class, $user);
-    $userForm->handleRequest($request);
-    if ($userForm->isSubmitted() && $userForm->isValid()) {
-        $plainPassword = $user->getPassword();
-
-        // find the encoder for the user
-        $encoder = $app['security.encoder_factory']->getEncoder($user);
-
-        // compute the encoded password
-        $password = $encoder->encodePassword($plainPassword, $user->getSalt());
-        $user->setPassword($password);
-
-        $app['dao.user']->save($user);
-        $app['session']->getFlashBag()->add('success', 'Le membre a été modifié avec succès.');
-    }
-
-    return $app['twig']->render('user_form.html.twig', array(
-        'title' => 'Modifier le membre',
-        'userForm' => $userForm->createView()));
-})->bind('admin_user_edit');
+$app->match('/admin/user/{id}/edit', "MicroCMS\Controller\AdminController::editUserAction")->bind('admin_user_edit');
 
 // Remove a user
-$app->get('/admin/user/{id}/delete', function($id, Request $request) use ($app) {
-    // Delete the user
-    $app['dao.user']->delete($id);
-    $app['session']->getFlashBag()->add('success', 'Le membre a été supprimé avec succès.');
-
-    // Redirect to admin home page
-    return $app->redirect($app['url_generator']->generate('admin'));
-})->bind('admin_user_delete');
+$app->get('/admin/user/{id}/delete', "MicroCMS\Controller\AdminController::deleteUserAction")->bind('admin_user_delete');
---
+++ src/Controller/AdminController.php
@@ -0,0 +1,219 @@
+<?php
+namespace MicroCMS\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use MicroCMS\Domain\Article;
+use MicroCMS\Domain\User;
+use MicroCMS\Form\Type\ArticleType;
+use MicroCMS\Form\Type\CommentType;
+use MicroCMS\Form\Type\UserType;
+
+class AdminController {
+    /**
+     * Admin home page controller.
+     *
+     * @param Application $app Silex application
+     */
+    public function indexAction(Application $app) {
+        $articles = $app['dao.article']->findAll();
+        $comments = $app['dao.comment']->findAll();
+        $users = $app['dao.user']->findAll();
+
+        return $app['twig']->render('admin.html.twig', array(
+            'articles' => $articles,
+            'comments' => $comments,
+            'users' => $users
+        ));
+    }
+
+
+
+
+    /**
+     * Add article controller.
+     *
+     * @param Request $request Incoming request
+     * @param Application $app Silex application
+     */
+    public function addArticleAction(Request $request, Application $app) {
+        $article = new Article();
+
+        $articleForm = $app['form.factory']->create(ArticleType::class, $article);
+        $articleForm->handleRequest($request);
+        if ($articleForm->isSubmitted() && $articleForm->isValid()) {
+            $app['dao.article']->save($article);
+            $app['session']->getFlashBag()->add('success', 'L\'article a été créé avec succès.');
+        }
+
+        return $app['twig']->render('article_form.html.twig', array(
+            'title' => 'Créer un article',
+            'articleForm' => $articleForm->createView()
+        ));
+    }
+
+    /**
+     * Edit article controller.
+     *
+     * @param integer $id Article id
+     * @param Request $request Incoming request
+     * @param Application $app Silex application
+     */
+
+    public function editArticleAction($id, Request $request, Application $app) {
+        $article = $app['dao.article']->find($id);
+
+        $articleForm = $app['form.factory']->create(ArticleType::class, $article);
+        $articleForm->handleRequest($request);
+        if ($articleForm->isSubmitted() && $articleForm->isValid()) {
+            $app['dao.article']->save($article);
+            $app['session']->getFlashBag()->add('success', 'L\'article a été modifié avec succès.');
+        }
+
+        return $app['twig']->render('article_form.html.twig', array(
+            'title' => 'Modifier l\'article',
+            'articleForm' => $articleForm->createView()
+        ));
+    }
+
+    /**
+     * Delete article controller.
+     *
+     * @param integer $id Article id
+     * @param Application $app Silex application
+     */
+    public function deleteArticleAction($id, Application $app) {
+        // Delete the article
+        $app['dao.article']->delete($id);
+        $app['session']->getFlashBag()->add('success', 'L\'article a été supprimé avec succès.');
+
+        // Redirect to admin home page
+        return $app->redirect($app['url_generator']->generate('admin'));
+    }
+
+
+
+
+    /**
+     * Edit comment controller.
+     *
+     * @param integer $id Comment id
+     * @param Request $request Incoming request
+     * @param Application $app Silex application
+     */
+    public function editCommentAction($id, Request $request, Application $app) {
+        $comment = $app['dao.comment']->find($id);
+
+        $commentForm = $app['form.factory']->create(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $app['dao.comment']->save($comment);
+            $app['session']->getFlashBag()->add('success', 'Le commentaire a été modifié avec succès.');
+        }
+
+        return $app['twig']->render('comment_form.html.twig', array(
+            'title' => 'Modifier le commentaire',
+            'commentForm' => $commentForm->createView()
+        ));
+    }
+
+    /**
+     * Delete comment controller.
+     *
+     * @param integer $id Comment id
+     * @param Application $app Silex application
+     */
+    public function deleteCommentAction($id, Application $app) {
+        $app['dao.comment']->delete($id);
+        $app['session']->getFlashBag()->add('success', 'Le commentaire a été supprimé avec succès.');
+
+        // Redirect to admin home page
+        return $app->redirect($app['url_generator']->generate('admin'));
+    }
+
+
+
+
+    /**
+     * Add user controller.
+     *
+     * @param Request $request Incoming request
+     * @param Application $app Silex application
+     */
+    public function addUserAction(Request $request, Application $app) {
+        $user = new User();
+
+        $userForm = $app['form.factory']->create(UserType::class, $user);
+        $userForm->handleRequest($request);
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            // generate a random salt value
+            $salt = substr(md5(time()), 0, 23);
+
+            $user->setSalt($salt);
+
+            $plainPassword = $user->getPassword();
+
+            // find the default encoder
+            $encoder = $app['security.encoder.bcrypt'];
+
+            // compute the encoded password
+            $password = $encoder->encodePassword($plainPassword, $user->getSalt());
+            $user->setPassword($password);
+
+            $app['dao.user']->save($user);
+            $app['session']->getFlashBag()->add('success', 'Le membre a été créé avec succès.');
+        }
+
+        return $app['twig']->render('user_form.html.twig', array(
+            'title' => 'Nouveau membre',
+            'userForm' => $userForm->createView()
+        ));
+    }
+
+    /**
+     * Edit user controller.
+     *
+     * @param integer $id User id
+     * @param Request $request Incoming request
+     * @param Application $app Silex application
+     */
+    public function editUserAction($id, Request $request, Application $app) {
+        $user = $app['dao.user']->find($id);
+
+        $userForm = $app['form.factory']->create(UserType::class, $user);
+        $userForm->handleRequest($request);
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $plainPassword = $user->getPassword();
+
+            // find the encoder for the user
+            $encoder = $app['security.encoder_factory']->getEncoder($user);
+
+            // compute the encoded password
+            $password = $encoder->encodePassword($plainPassword, $user->getSalt());
+            $user->setPassword($password);
+
+            $app['dao.user']->save($user);
+            $app['session']->getFlashBag()->add('success', 'Le membre a été modifié avec succès.');
+        }
+
+        return $app['twig']->render('user_form.html.twig', array(
+            'title' => 'Modifier le membre',
+            'userForm' => $userForm->createView()
+        ));
+    }
+
+    /**
+     * Delete user controller.
+     *
+     * @param integer $id User id
+     * @param Application $app Silex application
+     */
+    public function deleteUserAction($id, Application $app) {
+        // Delete the user
+        $app['dao.user']->delete($id);
+        $app['session']->getFlashBag()->add('success', 'Le membre a été supprimé avec succès.');
+
+        // Redirect to admin home page
+        return $app->redirect($app['url_generator']->generate('admin'));
+    }
+}
